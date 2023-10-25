9b012d1c573be9de4efdbdc3382ba5c0d87adeb8
Dmytro-Doronin <doronindmytro623@gmail.com>
2023-10-25 21:35:18 +0200

    Form + sql

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+    
+    if(isset($_POST['submit'])) {
+
+        $name = $_POST['name'];
+        $password = $_POST['password'];
+            
+        $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
+
+        if ($connection) {
+            echo "Connect";
+        } else {
+            die('Not connect');
+        }
+
+        $query = "INSERT INTO users(username,password) ";
+        $query .= "VALUES ('$name', '$password')";
+
+        $result = mysqli_query($connection, $query);
+
+        if(!$result) {
+            die("Query FAIL");
+        }
+
+    }
+?>
+
+<?php echo "<link rel='stylesheet' href='style.css'>";?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+</head>
+<body>
+
+    <form class="form" action="index.php" method="post">
+        <input class="textfield" name="name" type="text" placeholder="Name">
+        <input class="textfield" name="password" type="password" placeholder="Password">
+        <button class="button" type="submit" name="submit">Submit</button>
+    </form>
+
+</body>
+</html>
